405b01db24110eaf95d2df702949cac3bb14e322
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2025-11-05 18:29:10 +0100

    new

---
+++ js/controllers/cartController.js
@@ -0,0 +1,35 @@
+import{getCartList, removeFromCart} from "../models/cartmodel.js"
+import { isLoggedIn } from "../services/auth.js"
+import{Div}from "../views/atoms/index.js"
+import{cartListHeaderView, cartListView, cartTotalView} from "../views/organisms/cartViews.js"
+import{Layout} from "./layoutController.js"
+
+export const CartPage = async () => {
+    if(!isLoggedIn()){
+        location.href ='/index.htm#/login'
+        return false
+    }
+    cost data = await getcartList()
+
+    const arrHeaderColums = [
+        {name: 'Antal', className:'w-[10%] font-bold'},
+        {name: 'Produkt', className:'w-[60%] font-bold'},
+        {name: 'Pris', className: 'w-[20%] font-bold'},
+        {name: 'Handling', className: 'w-[10%] font-bold'},
+    ]
+    const totalPrice = data.reduce((sum, item)=>{
+        return sum + (item?.product?.price * item?.quantity || 0)
+    }, 0)
+    
+    const html = Div()
+    html.append(cartListHeaderView(arrHeaderColums))
+    html.append(cartListView(data))
+    html.append(cartTotalView(totalPrice))
+    attachCartListEvents(html)
+    
+    return Layout('Indkøbskurv',html)
+}
+
+const attachCartListEvents = (container) => {
+
+}
